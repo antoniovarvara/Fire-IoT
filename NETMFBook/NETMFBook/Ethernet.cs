0d1c837735b5ab4212ae205c39282c67549be0b9
Raffaele <raffysommy@gmail.com>
2018-04-05 16:44:44 +0200

    ethernet e mqtt

---
+++ NETMFBook/NETMFBook/Ethernet.cs
@@ -0,0 +1,98 @@
+﻿using Gadgeteer.Modules.GHIElectronics;
+using Microsoft.SPOT;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace NETMFBook
+{
+    class Ethernet
+    {
+        private AutoResetEvent waitHandle = new AutoResetEvent(false);
+        private EthernetJ11D ethernetJ11D;
+        private Thread t;
+        private Mqtt mqtt=null;
+        public Mqtt MQTT { get{
+            while (mqtt == null) {
+                waitHandle.WaitOne();
+            }
+            lock (this)
+            {
+                return mqtt;
+            }
+        }}
+
+        public Ethernet(EthernetJ11D ethernetJ11D) {
+            this.ethernetJ11D = ethernetJ11D;
+            t = new Thread(() => {
+                try
+                {
+
+                    //ethernetJ11D.NetworkSettings.PhysicalAddress=new byte[]{0x00,0x21,0x03,0x80,0x8B,0x6B};
+                    if (!ethernetJ11D.NetworkInterface.Opened)
+                    {
+                        ethernetJ11D.NetworkInterface.Open();
+                    }
+                    ethernetJ11D.NetworkDown += ethernetJ11D_NetworkDown;
+                    ethernetJ11D.UseThisNetworkInterface();
+                    ethernetJ11D.UseStaticIP("192.168.3.99", "255.255.255.0", "192.168.3.235");
+                    while (ethernetJ11D.NetworkInterface.IPAddress.Equals("0.0.0.0"))
+                    {
+                        Debug.Print("Waiting for Network!");
+                        Thread.Sleep(1000);
+                    }
+                    Debug.Print("Connected IP:" + ethernetJ11D.NetworkInterface.IPAddress);
+                    PrintNetworkState();
+                    StatusLed.led.SetLed(2, true);
+                    Thread.Sleep(2000);
+                    lock (this)
+                    {
+                        mqtt = new Mqtt();
+                        waitHandle.Set();
+                    }
+                    ethernetJ11D.NetworkUp += ethernetJ11D_NetworkUp;
+                }
+                catch (Exception e) {
+                    Debug.Print(e.StackTrace);
+                }
+            });
+            t.Start();
+        }
+
+        void ethernetJ11D_NetworkUp(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
+        {
+            StatusLed.led.SetLed(1, true);
+            Debug.Print("Network UP!");
+            PrintNetworkState();
+            mqtt.connectInfinite();
+        }
+
+        void ethernetJ11D_NetworkDown(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
+        {
+            StatusLed.led.SetLed(1, false);
+            Debug.Print("Network DOWN!");
+            PrintNetworkState();
+
+        }
+        private void PrintNetworkState()
+        {
+	        StringBuilder builder = new StringBuilder();
+	        builder.Append("Up=");
+	        builder.Append(ethernetJ11D.IsNetworkUp);
+	        builder.Append("; ");
+	        builder.Append("Connected=");
+	        builder.Append(ethernetJ11D.IsNetworkConnected);
+	        builder.Append("; ");
+	        builder.Append("IP=");
+	        builder.Append(ethernetJ11D.NetworkInterface.IPAddress);
+	        builder.Append("; ");
+	        builder.Append("Mask=");
+	        builder.Append(ethernetJ11D.NetworkInterface.SubnetMask);
+	        builder.Append("; ");
+	        builder.Append("GW=");
+	        builder.Append(ethernetJ11D.NetworkInterface.GatewayAddress);
+	        Debug.Print(builder.ToString());
+        }
+    }
+
+}
---
+++ NETMFBook/NETMFBook/Mqtt.cs
@@ -0,0 +1,84 @@
+using System;
+using Microsoft.SPOT;
+using uPLibrary.Networking.M2Mqtt;
+using System.Text;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Threading;
+
+namespace NETMFBook
+{
+    public class Mqtt
+    {
+        private IPAddress EndPoint = IPAddress.Parse("52.57.156.220");
+        private MqttClient client;
+        public event uPLibrary.Networking.M2Mqtt.MqttClient.MqttMsgPublishEventHandler PublishEvent;
+        public Mqtt() {
+            client = new MqttClient(EndPoint);
+            client.ConnectionClosed += client_ConnectionClosed;
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            connectInfinite();
+        }
+        public void connectInfinite() {
+            lock (this)
+            {
+                if (client.IsConnected == false)
+                {
+                    do
+                    {
+                        try
+                        {
+                            connect();
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(1000);
+                            continue;
+                        }
+                    } while (false);
+                }
+            }
+        }
+        private void connect() {
+            client.Connect("fez", "utente", "fezspiderII");
+            StatusLed.led.SetLed(3, true);
+        }
+
+        void client_ConnectionClosed(object sender, EventArgs e)
+        {
+            StatusLed.led.SetLed(3, false);
+            connectInfinite();
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            if (PublishEvent != null) {
+ 
+                PublishEvent.Invoke(sender,e);
+            }
+        }
+        public ushort Subscribe(String topic) {
+            if (client.IsConnected == false)
+            {
+                this.connectInfinite();
+            }
+            return client.Subscribe(new String[]{topic}, new byte[] {MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE});
+        }
+
+        public ushort Publish(String Topic, String Message)
+        {
+            try
+            {
+                if (client.IsConnected == false)
+                {
+                    this.connectInfinite();
+                }
+                return client.Publish(Topic, Encoding.UTF8.GetBytes(Message), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, true);
+            }
+            catch(Exception e) {
+                Debug.Print(e.StackTrace);
+                return 0;
+            }
+        }
+    }
+}
